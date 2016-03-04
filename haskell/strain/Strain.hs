822bdf32a0b0699a4b060a7408b14bb54132ea45
Marcell Jusztin <marcell.jusztin@flashtalking.com>
2016-03-04 14:49:35 +0000

    Trying out haskell-stylish

--- haskell/strain/Strain.hs
+++ haskell/strain/Strain.hs
@@ -3,10 +3,10 @@ module Strain where
 import Data.List
 
 keep :: (Eq a) => (a -> Bool) -> [a] -> [a]
-keep f [] = []
+keep _ [] = []
 keep f (x:xs)
-    | f x       = [x] ++ (keep f xs)
+    | f x       = x : keep f xs
     | otherwise = keep f xs
 
 discard :: (Eq a) => (a -> Bool) -> [a] -> [a]
-discard f x = x \\ (keep f x)
+discard f x = x \\ keep f x
